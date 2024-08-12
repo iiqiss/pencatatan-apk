e8b999df07805b4389e17d27f2d000ccb0ff001f
iiqiss <bilqisars.13@gmail.com>
2024-08-12 14:15:45 +0700

    install

---
+++ database/migrations/2024_08_12_032845_create_pengelola_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pengelola', function (Blueprint $table) {
+            $table->id('NIP');
+            $table->string('Nama_pengelola');
+            $table->integer('Kontak_pengelola');
+            $table->string('Nama_dinas');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pengelola');
+    }
+};
---
+++ database/migrations/2024_08_12_034417_create_data_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('data', function (Blueprint $table) {
+            $table->id('Id_data');
+            $table->integer('Tahun_pengumpulan');
+            $table->date('Tanggal_pengumpulan');
+            $table->string('Status_pengumpulan');
+            $table->string('Keterangan_pengumpulan');
+            $table->string('Judul_publikasi');
+            $table->text('Link_publikasi');
+            $table->string('Metadata');
+            $table->text('Link_metadata');
+            $table->string('rekomendasi');
+            $table->text('Link_rekomendasi');
+            $table->foreignid('NIP');
+            $table->foreign('NIP')->references('NIP')->on('pengelola');
+            $table->foreignid('Id_skpd');
+            $table->foreign('Id_skpd')->references('Id_skpd')->on('skpd');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('data');
+    }
+};
